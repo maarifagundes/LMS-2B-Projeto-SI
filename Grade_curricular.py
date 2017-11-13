81e8d1e6e6522c34d2ed2c4f3a2b2d3985eee238
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-11-13 17:32:38 -0200

    Delete Grade_curricular.py

--- Grade_curricular.py
+++
@@ -1,9 +0,0 @@
- class Gradecurricular(models.Model):
-       id = models.SmallIntegerField(primary_key=True)
-       id_curso = models.ForeignKey(Curso, models.DO_NOTHING, db_column='id_curso', blank=True, null=True)
-       ano = models.SmallIntegerField(unique=True, blank=True, null=True)
-       semestre = models.CharField(unique=True, max_length=1, blank=True, null=True)
-   
-       class Meta:
-           managed = False
-           db_table = 'GradeCurricular'
