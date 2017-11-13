a3267f0106fc12e27a68659c1a9c1bb54fddd5bf
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-11-13 17:33:02 -0200

    Delete Periodo_disciplina.py

--- Periodo_disciplina.py
+++
@@ -1,8 +0,0 @@
-class Periododisciplina(models.Model):
-       id = models.SmallIntegerField(primary_key=True)
-       id_periodo = models.ForeignKey(Periodo, models.DO_NOTHING, db_column='id_periodo', blank=True, null=True)
-       id_disciplina = models.ForeignKey(Disciplina, models.DO_NOTHING, db_column='id_disciplina', blank=True, null=True)
-   
-       class Meta:
-           managed = False
-           db_table = 'PeriodoDisciplina'
