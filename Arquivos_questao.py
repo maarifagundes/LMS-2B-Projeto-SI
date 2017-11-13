6a81ec5d0be94a85d51fd1f727978dc1336204a8
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-11-13 17:30:30 -0200

    Delete Arquivos_questao.py

--- Arquivos_questao.py
+++
@@ -1,9 +0,0 @@
-class Arquivosquestao(models.Model):
-       id = models.SmallIntegerField(primary_key=True)
-       id_questao = models.ForeignKey('Questao', models.DO_NOTHING, db_column='id_questao', blank=True, null=True)
-       arquivo = models.CharField(unique=True, max_length=500, blank=True, null=True)
-   
-       class Meta:
-           managed = False
-           db_table = 'ArquivosQuestao'
-   
