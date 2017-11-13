f14b1958349a6b2607c669a589d89ebf9728ae4a
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-11-13 17:31:18 -0200

    Delete Curso.py

--- Curso.py
+++
@@ -1,9 +0,0 @@
- class Curso(models.Model):
-       id = models.SmallIntegerField(primary_key=True)
-       sigla = models.CharField(unique=True, max_length=5, blank=True, null=True)
-       nome = models.CharField(unique=True, max_length=50, blank=True, null=True)
-   
-       class Meta:
-           managed = False
-           db_table = 'Curso'
-   
