9c6a5d64ae598838c38d7af9b62ce9fdc18b72fb
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-11-13 17:30:49 -0200

    Delete Arquivos_respostas.py

--- Arquivos_respostas.py
+++
@@ -1,8 +0,0 @@
- class Arquivosresposta(models.Model):
-       id = models.SmallIntegerField(primary_key=True)
-       id_resposta = models.ForeignKey('Resposta', models.DO_NOTHING, db_column='id_resposta', blank=True, null=True)
-       arquivo = models.CharField(unique=True, max_length=500, blank=True, null=True)
-   
-       class Meta:
-           managed = False
-           db_table = 'ArquivosResposta'
