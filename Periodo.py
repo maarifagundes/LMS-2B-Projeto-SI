669df37decfd7447fbcb205e9fb22288e5e43175
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-11-13 17:32:52 -0200

    Delete Periodo.py

--- Periodo.py
+++
@@ -1,8 +0,0 @@
- class Periodo(models.Model):
-       id = models.SmallIntegerField(primary_key=True)
-       id_grade = models.ForeignKey(Gradecurricular, models.DO_NOTHING, db_column='id_grade', blank=True, null=True)
-       numero = models.SmallIntegerField(unique=True, blank=True, null=True)
-   
-       class Meta:
-           managed = False
-           db_table = 'Periodo'
