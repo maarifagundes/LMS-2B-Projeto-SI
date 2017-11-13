6db6be50bd216fa9cad976744f039ecd5bc6ce75
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-11-13 17:31:31 -0200

    Delete Curso_turma.py

--- Curso_turma.py
+++
@@ -1,8 +0,0 @@
-class Cursoturma(models.Model):
-       id = models.SmallIntegerField(primary_key=True)
-       id_turma = models.ForeignKey('Turma', models.DO_NOTHING, db_column='id_turma', blank=True, null=True)
-       id_curso = models.ForeignKey(Curso, models.DO_NOTHING, db_column='id_curso', blank=True, null=True)
-   
-       class Meta:
-           managed = False
-           db_table = 'CursoTurma'
